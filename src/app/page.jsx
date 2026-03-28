4570ef4e322d4be483efe38374008b97bbf904d8
oliullahakib <oliullahakib@gmail.com>
2026-03-28 15:40:31 +0600

    initialize the project with daisy ui installation

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-accent">Accent</button>
+    <button className="btn btn-info">Info</button>
+    <button className="btn btn-success">Success</button>
+    <button className="btn btn-warning">Warning</button>
+    <button className="btn btn-error">Error</button>
+    </>
+  );
+}
